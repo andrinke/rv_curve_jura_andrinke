9e10fcf955ae24c950f804ff5802d269f53d0116
Andrin Kessler <andrin.mattia@gmail.com>
2023-05-09 12:22:18 +0200

    remove long description from setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ setuptools.setup(
     author_email="andrin.mattia@gmail.com",
     description="Plot all RV curves you want!",
     packages=setuptools.find_packages(),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/andrinke/rv_curve_jura_andrinke",
     install_requires=["numpy","matplotlib"],
     classifiers=["Programming Language :: Python :: 3"],
